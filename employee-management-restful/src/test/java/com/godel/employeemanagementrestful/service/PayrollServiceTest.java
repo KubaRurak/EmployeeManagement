eaf68e6701cce1f819ba946008cabf3f656f5fe8
KubaRurak <69719179+KubaRurak@users.noreply.github.com>
2023-09-24 19:47:12 +0200

    added some tests

--- employee-management-restful/src/test/java/com/godel/employeemanagementrestful/service/PayrollServiceTest.java
+++ employee-management-restful/src/test/java/com/godel/employeemanagementrestful/service/PayrollServiceTest.java
@@ -5,19 +5,20 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.time.LocalDateTime;
 import java.time.YearMonth;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.godel.employeemanagementrestful.mapper.WorkOrderMapperImpl;
+import com.godel.employeemanagementrestful.repository.CustomerRepository;
+import com.godel.employeemanagementrestful.repository.PayrollRepository;
+import com.godel.employeemanagementrestful.repository.UserRepository;
+import com.godel.employeemanagementrestful.repository.WorkOrderRepository;
+
 @SpringBootTest
 class PayrollServiceTest {
-	@Autowired
-	PayrollService payrollService;
-
-	@Test
-	void generatePayrollTest(){
-		YearMonth thisYearMonth = YearMonth.of(2023, 2);
-		payrollService.generatePayrollForAll(YearMonth.of(2022, 2),24);
-	}
-
 }
